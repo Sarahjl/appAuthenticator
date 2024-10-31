8088a9e8d70d55e87d04fbb0f0873cdaf219b883
Sarahjl <sjandoza18@gmail.com>
2024-10-30 21:38:08 -0300

    aa

--- app/src/main/java/com/example/appauthenticator/MainActivity.kt
+++ app/src/main/java/com/example/appauthenticator/MainActivity.kt
@@ -1,68 +1,217 @@
 package com.example.appauthenticator
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.appauthenticator.ui.theme.AppAuthenticatorTheme
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.auth
-
-private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val auth = Firebase.auth
-
-        Log.i(TAG, "onCreate usuário atual: ${auth.currentUser}")
-
-        auth.createUserWithEmailAndPassword(
-            "james.francis.byrnes@example-pet-store.com",
-            "123456"
-        ).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Log.i(TAG, "onCreate: Sucesso")
-            } else {
-                Log.i(TAG, "onCreate: Falha -> ${task.exception}")
-            }
-        }
-
-        enableEdgeToEdge()
         setContent {
             AppAuthenticatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isLoggedIn by remember { mutableStateOf(false) }
+    val auth = Firebase.auth
+    val context = LocalContext.current
+
+    val isButtonEnabled = email.isNotEmpty() && password.isNotEmpty()
+
+    if (isLoggedIn) {
+        AuthenticationScreen(auth) { isLoggedIn = false }
+    } else {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text("Login") })
+            },
+            content = { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    TextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    TextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        modifier = Modifier.fillMaxWidth(),
+                        visualTransformation = PasswordVisualTransformation()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            auth.signInWithEmailAndPassword(email, password)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(context, "Login realizado!", Toast.LENGTH_SHORT).show()
+                                        isLoggedIn = true
+                                    } else {
+                                        val exception = task.exception
+                                        when (exception) {
+                                            is FirebaseAuthInvalidCredentialsException -> {
+                                                Toast.makeText(context, "Email e/ou senha incorretos.", Toast.LENGTH_SHORT).show()
+                                            }
+                                            is FirebaseAuthInvalidUserException -> {
+                                                Toast.makeText(context, "Usuário não encontrado.", Toast.LENGTH_SHORT).show()
+                                            }
+                                            else -> {
+                                                Toast.makeText(context, "Erro no login: ${exception?.message}", Toast.LENGTH_SHORT).show()
+                                            }
+                                        }
+                                    }
+                                }
+                        },
+                        enabled = isButtonEnabled,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Login")
+                    }
+                }
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AuthenticationScreen(auth: FirebaseAuth, onLogout: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    val isButtonEnabled = email.isNotEmpty() && password.isNotEmpty()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Cadastro de Usuários") })
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") },
+                    modifier = Modifier.fillMaxWidth(),
+                    visualTransformation = PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Toast.makeText(context, "Usuário cadastrado", Toast.LENGTH_SHORT).show()
+                                    email = ""
+                                    password = ""
+                                } else {
+                                    val exception = task.exception
+                                    when (exception) {
+                                        is FirebaseAuthWeakPasswordException -> {
+                                            Toast.makeText(context, "Senha muito fraca. Escolha uma senha mais forte.", Toast.LENGTH_SHORT).show()
+                                        }
+                                        is FirebaseAuthInvalidCredentialsException -> {
+                                            Toast.makeText(context, "Email mal formatado.", Toast.LENGTH_SHORT).show()
+                                        }
+                                        is FirebaseAuthUserCollisionException -> {
+                                            Toast.makeText(context, "Este email já está em uso.", Toast.LENGTH_SHORT).show()
+                                        }
+                                        else -> {
+                                            Toast.makeText(context, "Erro ao cadastrar: ${exception?.message}", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                                }
+                            }
+                    },
+                    enabled = isButtonEnabled,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Cadastrar")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        auth.signOut()
+                        onLogout()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Logout")
+                }
+            }
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginScreenPreview() {
     AppAuthenticatorTheme {
-        Greeting("Android")
+        LoginScreen()
     }
 }
